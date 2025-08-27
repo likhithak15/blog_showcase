75c8d0d7a3c19c8456dcc8f0a6dea80a0f4772cc
KOTTAKOTA LIKHITHA <likhithakottakota83@gmail.com>
2025-08-27 16:01:19 +0530

    Create blogs.js

---
+++ data/blogs.js
@@ -0,0 +1,16 @@
+const blogs = [
+  {
+    id: 1,
+    title: "First Blog Post",
+    excerpt: "This is a short description of the first blog post.",
+    image: "/blog1.jpg"
+  },
+  {
+    id: 2,
+    title: "Second Blog Post",
+    excerpt: "A quick preview of the second blog post content.",
+    image: "/blog1.jpg"
+  }
+];
+
+export default blogs;
