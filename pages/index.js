d2b661eab0d2e6a05c270eec3cede386028b24a5
KOTTAKOTA LIKHITHA <likhithakottakota83@gmail.com>
2025-08-27 18:30:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import BlogCard from "@/components/BlogCard";
-import blogs from "@/data/blogs";
+import BlogCard from '../components/BlogCard';
+import blogs from '../data/blogs';
 
 export default function Home() {
   return (
-    <main className="max-w-4xl mx-auto p-6">
-      <h1 className="text-4xl font-bold mb-6 text-center">
-        🚀 My Blog Showcase
-      </h1>
-      <div className="grid md:grid-cols-2 gap-6">
+    <div className="max-w-6xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">My Blog</h1>
+      <div className="grid md:grid-cols-3 gap-6">
         {blogs.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} />
+          <BlogCard
+            key={blog.id}
+            title={blog.title}
+            description={blog.description}
+            image={blog.image}
+          />
         ))}
       </div>
-    </main>
+    </div>
   );
 }
+
