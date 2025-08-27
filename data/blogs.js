38f391d250076810b0d37abb1a09f56d48f2ed66
KOTTAKOTA LIKHITHA <likhithakottakota83@gmail.com>
2025-08-27 18:29:51 +0530

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -1,16 +1,23 @@
 const blogs = [
   {
     id: 1,
-    title: "First Blog Post",
-    excerpt: "This is a short description of the first blog post.",
-    image: "/blog1.jpg"
+    title: "Getting Started with Next.js",
+    description: "Learn the basics of Next.js and why it's a powerful framework for React developers.",
+    image: "/blog1.jpg",
   },
   {
     id: 2,
-    title: "Second Blog Post",
-    excerpt: "A quick preview of the second blog post content.",
-    image: "/blog1.jpg"
-  }
+    title: "Tailwind CSS Tips",
+    description: "Useful tips and tricks to get the most out of Tailwind CSS in your projects.",
+    image: "/blog2.jpg",
+  },
+  {
+    id: 3,
+    title: "Deploying on Vercel",
+    description: "Step-by-step guide to deploying your Next.js app seamlessly on Vercel.",
+    image: "/blog3.jpg",
+  },
 ];
 
 export default blogs;
+
